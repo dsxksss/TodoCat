e406b3c57f520bb79ab3a4f40d1a5232a730db42
VentCat <2546650292@qq.com>
2023-08-21 00:20:31 +0800

    添加AddTodoDialog

--- lib/app/pages/home/widgets/add_todo_card_btn.dart
+++ lib/app/pages/home/widgets/add_todo_card_btn.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:todo_cat/app/data/schemas/task.dart';
@@ -13,24 +15,7 @@ class AddTodoCardBtn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => {
-        ctrl.addTodo(
-          task,
-          Todo(
-            id: 1,
-            title: "新增Todo",
-            createdAt: DateTime.now().millisecondsSinceEpoch,
-            tags: [
-              "默认awada",
-              "自带adawda",
-              "默认",
-              "自带a",
-              "默认a",
-              "自带xaw",
-            ],
-          ),
-        )
-      },
+      onTap: () => {showAddTodoDialog(task)},
       child: Container(
         width: 1.sw,
         margin: EdgeInsets.only(left: 20.w, right: 20.w, bottom: 20.w),
@@ -65,4 +50,115 @@ class AddTodoCardBtn extends StatelessWidget {
       ),
     );
   }
+}
+
+class AddTodoDialog extends StatelessWidget {
+  AddTodoDialog({
+    super.key,
+    required this.task,
+  });
+
+  final HomeController ctrl = Get.find();
+  final Task task;
+  final formKey = GlobalKey<FormState>();
+  final editCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 0.6.sw,
+        height: 600.w,
+        padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 20.w),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey.shade300),
+          borderRadius: BorderRadius.circular(15.r),
+        ),
+        child: SizedBox.expand(
+          child: Form(
+            key: formKey,
+            child: Material(
+              type: MaterialType.transparency,
+              child: ListView(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "addTodo".tr,
+                        style: TextStyle(
+                            fontSize: 30.sp, fontWeight: FontWeight.bold),
+                      ),
+                      TextButton(
+                        child: Text("done".tr),
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            ctrl.addTodo(
+                              task,
+                              Todo(
+                                id: 1,
+                                title: editCtrl.text,
+                                createdAt:
+                                    DateTime.now().millisecondsSinceEpoch,
+                                tags: [
+                                  "默认awada",
+                                  "自带adawda",
+                                  "默认",
+                                  "自带a",
+                                  "默认a",
+                                  "自带xaw",
+                                ],
+                              ),
+                            );
+
+                            Get.back();
+                          }
+                        },
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20.w,
+                  ),
+                  TextFormField(
+                    controller: editCtrl,
+                    decoration: InputDecoration(
+                        isDense: false,
+                        fillColor: Colors.grey,
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20.w),
+                        hintText: "Title",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        focusColor: Colors.green
+
+                        // prefixIcon: Icon(Icons.title_rounded),
+                        ),
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return "请填写完整内容";
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void showAddTodoDialog(Task task) {
+  Get.generalDialog(
+    barrierLabel: "Barrier",
+    barrierDismissible: true,
+    barrierColor: Colors.black.withOpacity(0.5),
+    transitionDuration: 250.ms,
+    pageBuilder: (_, __, ___) {
+      return AddTodoDialog(task: task);
+    },
+  );
 }
