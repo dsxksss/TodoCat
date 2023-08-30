3af3cb8121a8956a2511921e9472d14f62b115ed
VentCat <2546650292@qq.com>
2023-08-30 17:28:15 +0800

    添加 AppController

--- lib/app.dart
+++ lib/app.dart
@@ -2,13 +2,29 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:todo_cat/data/schemas/local_notice.dart';
 import 'package:todo_cat/locales/locales.dart';
 import 'package:todo_cat/manager/local_notification_manager.dart';
 import 'package:todo_cat/pages/unknown.dart';
 import 'package:todo_cat/routers/router_map.dart';
 import 'package:todo_cat/themes/light_theme.dart';
-import 'package:uuid/uuid.dart';
+
+class AppController extends GetxController {
+  late final LocalNotificationManager localNotificationManager;
+
+  @override
+  void onInit() async {
+    localNotificationManager = await LocalNotificationManager.getInstance();
+    localNotificationManager.checkAllLocalNotification();
+
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    localNotificationManager.destroyLocalNotification();
+    super.onClose();
+  }
+}
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -18,31 +34,13 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  late final LocalNotificationManager localNotificationManager;
-
-  void initNotification() async {
-    localNotificationManager = await LocalNotificationManager.getInstance();
-    localNotificationManager.checkAllLocalNotification();
-    final LocalNotice notice = LocalNotice(
-        id: const Uuid().v4(),
-        title: "任务已经失效",
-        description: "您有一个任务已经失效:修bug",
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        remindersAt:
-            DateTime.now().add(Duration(seconds: 10)).millisecondsSinceEpoch);
-    localNotificationManager.saveNotification(notice.id, notice);
-  }
+  late final AppController controller;
 
   @override
   void initState() {
-    initNotification();
+    Get.put(AppController());
+    controller = Get.find();
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    localNotificationManager.destroyLocalNotification();
-    super.dispose();
   }
 
   @override
