b5ff5a6012a6b333dfdfedb5c199997e763af6e3
VentCat <2546650292@qq.com>
2023-09-19 23:28:44 +0800

    调整自定义toast样式

--- lib/widgets/show_toast.dart
+++ lib/widgets/show_toast.dart
@@ -1,43 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 
-void showToast(
-  String message, {
-  Duration? displayTime,
-  Duration? animationTime,
-  AlignmentGeometry? alignment,
-  Widget Function(BuildContext)? builder,
-}) {
+void showToast(String message,
+    {Duration? displayTime,
+    Duration? animationTime,
+    AlignmentGeometry? alignment,
+    Widget Function(BuildContext)? builder,
+    EdgeInsetsGeometry? margin,
+    Widget Function(AnimationController, Widget, AnimationParam)?
+        animationBuilder,
+    String? tag,
+    bool? keepSingle}) {
   SmartDialog.show(
-    displayTime: displayTime ?? 3000.ms,
-    animationTime: animationTime ?? 200.ms,
-    alignment: alignment ?? Alignment.topRight,
+    displayTime: displayTime ?? 4000.ms,
+    animationTime: animationTime ?? 1000.ms,
+    tag: tag,
+    keepSingle: keepSingle ?? true,
+    alignment: alignment ?? Alignment.bottomCenter,
     maskColor: Colors.transparent,
     maskWidget: Container(),
     clickMaskDismiss: false,
-    animationBuilder: (controller, child, _) => child
-        .animate(controller: controller)
-        .moveX(
-          begin: 20,
-          end: 0,
-          duration: controller.duration,
-        )
-        .fade(duration: controller.duration),
+    animationBuilder: animationBuilder ??
+        (controller, child, _) => child
+            .animate(controller: controller)
+            .moveY(
+              begin: 120,
+              end: 0,
+              duration: controller.duration,
+              curve: Curves.easeInOutBack,
+            )
+            .fade(
+              duration: controller.duration,
+              curve: Curves.easeInOut,
+            ),
     builder: builder ??
         (context) {
           return Container(
-            margin: const EdgeInsets.only(top: 50, right: 50),
-            width: 200,
+            margin: margin ?? const EdgeInsets.only(bottom: 100),
+            width: 250,
             height: 60,
             decoration: BoxDecoration(
-              color: Colors.lightBlue,
-              borderRadius: BorderRadius.circular(20),
+              color: context.theme.dialogBackgroundColor,
+              borderRadius: BorderRadius.circular(5),
+              boxShadow: [
+                BoxShadow(
+                  color: context.theme.dividerColor,
+                  blurRadius: context.isDarkMode ? 2 : 5,
+                )
+              ],
             ),
             child: Center(
-              child: Text(
-                message,
-                style: const TextStyle(color: Colors.white),
+              child: Row(
+                children: [
+                  const SizedBox(width: 20),
+                  const Icon(FontAwesomeIcons.check),
+                  const SizedBox(width: 20),
+                  Text(message),
+                ],
               ),
             ),
           );
