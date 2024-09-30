be80beab4eaed2b37aa3144a3a0ac4e997ed145b
VentCat <2546650292@qq.com>
2024-09-30 18:05:20 +0800

    fix 当修改完系统语言之后，退出app，打开设置页面出现显示异常问题

--- lib/pages/binding.dart
+++ lib/pages/binding.dart
@@ -6,7 +6,7 @@ class AppBinding implements Bindings {
   @override
   void dependencies() {
     Get.put(HomeController());
-    Get.put(SettingsController());
+    Get.lazyPut(() => SettingsController());
     Get.put(DatePickerController());
     Get.put(AddTodoDialogController());
   }
